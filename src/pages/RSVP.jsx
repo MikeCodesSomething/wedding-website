b07d967249c4cdb6b5386ba0272fd2b8c550b052
Mike Williams <MichaelJamesWilliams193@gmail.com>
2023-12-24 14:12:24 +0000

    Embed RSVP form

--- src/pages/RSVP.jsx
+++ src/pages/RSVP.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 const RSVP = () => {
   return (
-    <div className='container'>
-      RSVP
+    <div className='title-box'>
+      <h2>RSVP</h2>
+      <div className='padded'>
+        <p>Please RSVP using the form below, if this isn't 
+           loading try the <a href='https://forms.gle/4kjyMnNUWmjBDriL6'
+                              target='_blank'>
+                              direct link</a>
+        </p>
+      </div>
+      <iframe
+        title="RSVP Form"
+        src="https://docs.google.com/forms/d/e/1FAIpQLScbmEhxbqSioNsEUQL6CVEYc0x-QHk057YgXcDDt-dGdhYOgg/viewform?embedded=true"
+        width="450"
+        height="1350"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+      >
+        Loading…
+      </iframe>
     </div>
-  )
+  );
 }
 
-export default RSVP
+export default RSVP;
