a0c88cce5ba2ac26c7168a2f81c8aa19283268ae
Mike Williams <MichaelJamesWilliams193@gmail.com>
2024-01-07 11:05:14 +0000

    Add 12:00 start to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,9 @@ const Home = () => {
       <div className='title-box'>
         <h2>Tabitha & Mike</h2>
         <h4>Are getting married!</h4>
-        <h4>20th | July | 2024</h4>
+        <h4>20th | July | 2024
+          <p>12:00 pm</p>
+        </h4>
         <Link style={{padding: '10px' }} to="/rsvp">RSVP</Link>
       </div>
       <div className='container'>
