8ec169276a70421fd4a745b5c62bb9ddbdeefa5b
Mike Williams <MichaelJamesWilliams193@gmail.com>
2024-02-12 23:09:19 +0000

    Stop swiping of the carousel

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -1,38 +1,35 @@
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Photos = () => {
-
-    return (
-        <Carousel autoPlay interval={5000} infiniteLoop>
-            <div>
-                <img src="./Carousel/HayleyGellPhotography-2.jpg" />
-            </div>
-            <div>
-                <img src="./Carousel/HayleyGellPhotography-17.jpg" />
-            </div>
-            <div>
-                <img src="./Carousel/engagement-photo.jpg" />
-            </div>
-            <div>
-                <img src="./Carousel/HayleyGellPhotography-23.jpg" />
-            </div>
-            <div>
-                <img src="./Carousel/HayleyGellPhotography-32.jpg" />
-            </div>
-            <div>
-                <img src="./Carousel/HayleyGellPhotography-40.jpg" />
-            </div>
-            <div>
-                <img src="./Carousel/HayleyGellPhotography-49.jpg" />
-            </div>
-            <div>
-                <img src="./Carousel/HayleyGellPhotography-54.jpg" />
-            </div>
-        </Carousel>
-    )
-
-}
-
+  return (
+    <Carousel autoPlay interval={5000} infiniteLoop swipeable={false}>
+      <div>
+        <img src="./Carousel/HayleyGellPhotography-2.jpg" />
+      </div>
+      <div>
+        <img src="./Carousel/HayleyGellPhotography-17.jpg" />
+      </div>
+      <div>
+        <img src="./Carousel/engagement-photo.jpg" />
+      </div>
+      <div>
+        <img src="./Carousel/HayleyGellPhotography-23.jpg" />
+      </div>
+      <div>
+        <img src="./Carousel/HayleyGellPhotography-32.jpg" />
+      </div>
+      <div>
+        <img src="./Carousel/HayleyGellPhotography-40.jpg" />
+      </div>
+      <div>
+        <img src="./Carousel/HayleyGellPhotography-49.jpg" />
+      </div>
+      <div>
+        <img src="./Carousel/HayleyGellPhotography-54.jpg" />
+      </div>
+    </Carousel>
+  );
+};
 
 export default Photos;
